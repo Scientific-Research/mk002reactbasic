9d143c4b14fb04d6377b3ccf6b81c72e12d31a05
Maximilian <scientificresearch309@gmail.com>
2022-11-13 21:00:28 +0100

    german nouns showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import "./App.scss";
+import germanNouns from "./data/germanNouns.json";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -8,6 +9,18 @@ function App() {
     <div className="App">
       <h1>Test Site</h1>
       <p>This is a Test.</p>
+      <h2>There are {<span>{germanNouns.length}</span>} German Nouns</h2>
+      <h3 className="nouns">
+        {germanNouns.map((noun) => {
+          return (
+            <div className="noun">
+              <div>{`article: ${noun.article}`}</div>
+              <div>{`singular: ${noun.singular}`}</div>
+              <div>{`plural: ${noun.plural}`}</div>
+            </div>
+          );
+        })}
+      </h3>
     </div>
   );
 }
